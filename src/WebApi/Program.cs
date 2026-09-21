cb5a74e61ceb2f9a5d4e58380efad7c8ee04e199
Rch1k <forgd7408@gmail.com>
2026-09-21 12:33:20 +0300

    feat: add docker compose, unit tests and automated migrations

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -62,4 +62,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await dbContext.Database.MigrateAsync();
+}
+
 app.Run();
